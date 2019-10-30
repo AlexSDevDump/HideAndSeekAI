7bb49a3e643edf423b217ef0f839c9970d80ac65
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2019-10-30 17:21:41 +0000

    Random Colours

---
+++ Assets/Scripts/Environment/RandomColour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RandomColour : MonoBehaviour
+{
+    public Color randomColour;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomColour = GenerateColour();
+        SetColour(randomColour);
+    }
+    
+    Color GenerateColour()
+    {
+        Color colourBuffer = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        return colourBuffer;
+    }
+
+    void SetColour(Color colour)
+    {
+        GetComponent<Renderer>().material.color = colour;
+    }
+}
