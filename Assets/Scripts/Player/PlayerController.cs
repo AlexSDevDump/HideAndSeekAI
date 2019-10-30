10967e90eae75fe48457d1c8ad2972c76f9e8ba8
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2019-10-30 14:18:08 +0000

    Base Game

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Movement(Vector3 moveDir)
+    {
+        moveDir = moveDir.normalized * speed;
+        rb.MovePosition(rb.position + moveDir * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/UserInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    private Vector2 movement;
+    private PlayerController pc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        Vector3 finalMovement = new Vector3(movement.x, 0f, movement.y);
+        if (movement != Vector2.zero) { pc.Movement(finalMovement); }
+    }
+}
