73fcb4df7f47944b048be301410fedc915316bd1
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2019-11-14 13:36:11 +0000

    Cam Switch, AI Sensors

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera mainCam;
+    public List<Transform> camPos = new List<Transform>();
+    public int currentCamID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DefaultCamSetting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)) { MoveCamera(true); }
+        if(Input.GetKeyDown(KeyCode.Q)) { MoveCamera(false); }
+        if(Input.GetKeyDown(KeyCode.R)) { ResetTransform(); }
+    }
+
+    void DefaultCamSetting()
+    {
+        mainCam.transform.SetParent(camPos[0]);
+        ResetTransform();
+    }
+
+    void MoveCamera(bool right)
+    {
+        if(right)
+        {
+            currentCamID++;
+            if(currentCamID > camPos.Count - 1) { currentCamID = 0; }
+            mainCam.transform.SetParent(camPos[currentCamID]);
+        }
+
+        else
+        {
+            currentCamID--;
+            if (currentCamID < 0) { currentCamID = camPos.Count - 1; }
+            mainCam.transform.SetParent(camPos[currentCamID]);
+        }
+
+        ResetTransform();
+    }
+
+    void ResetTransform()
+    {
+        Transform t = mainCam.transform;
+        t.localPosition = Vector3.zero;
+        t.localRotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/Player/AI/AIController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    Rigidbody rb;
+    Collider col;
+
+
+    public float movement;
+    public float fitness;
+    public float[] inp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        col = GetComponentInChildren<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        Vector3[] sensors = new Vector3[]
+        {
+            transform.TransformDirection(Vector3.left),
+            transform.TransformDirection(Vector3.left + Vector3.forward),
+            transform.TransformDirection(Vector3.forward),
+            transform.TransformDirection(Vector3.right + Vector3.forward),
+            transform.TransformDirection(Vector3.right),
+        };
+
+        inp = new float[sensors.Length];
+
+        for (int i = 0; i < sensors.Length; i++)
+        {
+            if (Physics.Raycast(transform.position, sensors[i], out hit))
+            {
+                if (hit.collider != null && hit.collider != col)
+                {
+                    inp[i] = hit.distance;
+                }
+            }
+
+            Debug.DrawRay(transform.position, sensors[i] * 10, Color.red);
+        }
+    }
+}
